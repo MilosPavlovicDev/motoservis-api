78ee676084e847d4f0d4328d26372055980783da
pakidev(Milos Pavlovic) <milospavlovicdev@gmail.com>
2026-06-25 00:37:33 +0200

    Initial commit

---
+++ src/main/java/rs/metropolitan/motoservisapi/MotoservisapiApplication.java
@@ -0,0 +1,13 @@
+package rs.metropolitan.motoservisapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoservisapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotoservisapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rs/metropolitan/motoservisapi/MotoservisapiApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.metropolitan.motoservisapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotoservisapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
